70ef6cf5a72bffe98c5470acb867b622a20865d9
사랑 <136934013+ByzanDev@users.noreply.github.com>
2024-12-27 16:38:49 -0300

    fix: typo

--- twitch/commands/ping.js
+++ twitch/commands/ping.js
@@ -3,7 +3,6 @@ module.exports = {
     usage: ["!ping"],
     description: "Returns pong and args length",
     execute(bot, sender, args) {
-        console.log(sender)
         bot.sendMessage(`Pong! @${sender.username}, sua mensagem tem ${args.length} args`)
     }
 }
