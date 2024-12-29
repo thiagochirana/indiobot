ed63f1a1ea218374fbbede9b1116cd25a98c9465
Chirana <engsw.thiago@gmail.com>
2024-12-28 23:12:59 -0300

    feat: add other commands

--- twitch/commands/ping.js
+++ twitch/commands/ping.js
@@ -3,6 +3,7 @@ module.exports = {
     usage: ["!ping"],
     description: "Returns pong and args length",
     execute(bot, sender, args) {
+        console.log(sender)
         bot.sendMessage(`Pong! @${sender.username}, sua mensagem tem ${args.length} args`)
     }
 }
