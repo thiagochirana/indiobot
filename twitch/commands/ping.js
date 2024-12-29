3bce107d5b452757ea34006faabb7b912263018f
사랑 <136934013+ByzanDev@users.noreply.github.com>
2024-12-28 22:33:40 -0300

    fix: remove console.log

--- twitch/commands/ping.js
+++ twitch/commands/ping.js
@@ -3,7 +3,6 @@ module.exports = {
     usage: ["!ping"],
     description: "Returns pong and args length",
     execute(bot, interaction) {
-        console.log(interaction)
         bot.sendMessage(`Pong! @${interaction.sender.login}, sua mensagem tem ${interaction.message.args.length} args`)
     }
 }
